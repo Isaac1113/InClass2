b207fb9c8ee253347fc7826301447964f3a21064
Isaac Diaz <itdiaz@cpp.edu>
2021-09-30 23:23:35 -0700

    Set up prototype of checkMagicSquare function

---
+++ LoShuMagicSquare/mainMagicSquare.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int checkMagicSquare(int *arr, int i, int j);
+
+int main() {
+    int sqr1[3][3] = {{4, 9, 2},
+                        {3, 5, 7},
+                        {8, 1, 6}
+                        };
+
+    
+    
+
+    return EXIT_SUCCESS;
+}
+
+int checkMagicSquare(int *arr, int i, int j)
+{
+
+}
