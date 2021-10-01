e584084b66a53f3a5757166093dd30fe2a109044
Isaac Diaz <itdiaz@cpp.edu>
2021-09-30 22:29:51 -0700

    Add wrapper searchEmployeeByPhone

--- EmployeeSearch/employeeTwo.c
+++ EmployeeSearch/employeeTwo.c
@@ -48,3 +48,8 @@ PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int size, char *name)
 {
     return searchEmployeeTable(ptr, size, name, compareEmployeeName);
 }
+
+PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int size, char *phone)
+{
+    return searchEmployeeTable(ptr, size, phone, compareEmployeePhone);
+}
